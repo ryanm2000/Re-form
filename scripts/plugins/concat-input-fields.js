8d786693b0906753486b246c011871855d799f95
Ryan Margheriti <ryan.margheriti@carsales.com.au>
2014-01-14 17:46:50 +1100

    Misc small changes

--- scripts/plugins/concat-input-fields.js
+++ scripts/plugins/concat-input-fields.js
@@ -6,51 +6,53 @@
       return $('[data-replicate-source='+replicateGroup+']');
     }
 
+    function getDestinationField (replicateGroup) {
+      return $('[data-replicate-destination='+replicateGroup+']').first();
+    }
+
     function generateValue (sourceFields) {
       var vals = '';
       sourceFields.each(function(){
-          vals = vals + $(this).val();
+        vals = vals + $(this).val();
       });
       return vals;
     }
 
     function fieldAdvance(currentField) {
       if(!currentField.is(':last-child')) {
-          currentField.next().focus();
+        currentField.next().focus();
       }
     }
 
-    function destinationField (replicateGroup) {
-      return $('[data-replicate-destination='+replicateGroup+']').first();
-    }
 
     function copyToDestination (value, destinationField) {
-        destinationField.val(value)
+       destinationField.val(value)
     }
 
-    function init (replicateGroup) {
-      // Cache sourceFields
-      var sourceFields = getSourceFields(replicateGroup);
+    function init () {
+      $('[data-replicate-destination]').each(function() {
+        var replicateGroup = $(this).attr('data-replicate-destination'); // Unique name for group
+        // Cache sourceFields
+        var sourceFields = getSourceFields(replicateGroup);
 
-      // Wire up keypress and keyup events
-      sourceFields.keypress(function(e) {
-        var that = $(this);
-        if (that.val().length+1 == that.attr('maxlength') && e.which !== 0) { fieldAdvance(that); } // Advance to the next field
-      }).keyup(function() {
-        copyToDestination(
-          generateValue(sourceFields),
-          destinationField(replicateGroup)
-        ); // Copy text to the destination field
+        // Wire up keypress and keyup events
+        sourceFields.keypress(function(e) {
+          var that = $(this);
+          if (that.val().length+1 == that.attr('maxlength') && e.which !== 0) {
+            fieldAdvance(that); // Advance to the next field
+          }
+        }).keyup(function() {
+          copyToDestination(
+            generateValue(sourceFields),
+            getDestinationField(replicateGroup)
+          ); // Copy text to the destination field
+        });
       })
-
     }
 
   // If we are have replicate-destination present
-  if($('[data-replicate-destination]')) {
-    $('[data-replicate-destination]').each(function() {
-      var replicateGroup = $(this).attr('data-replicate-destination'); // Unique name for group
-      init(replicateGroup);
-    })
+  if($('[data-replicate-destination]').length) {
+    init();
   }
 
 }());
