b39f4fffdb95499ea28dd5cce4929f39cf9ffa5e
Ryan Margheriti <ryanmargheriti@gmail.com>
2014-02-06 18:58:38 +1100

    Removed reliance on jquery. Now a pure JS implementation

--- scripts/plugins/concat-input-fields.js
+++ scripts/plugins/concat-input-fields.js
@@ -1,58 +1,70 @@
-// Concatinate and replicate fields
 (function() {
   "use strict";
 
-    function getSourceFields (replicateGroup) {
-      return $('[data-replicate-source='+replicateGroup+']');
-    }
-
-    function getDestinationField (replicateGroup) {
-      return $('[data-replicate-destination='+replicateGroup+']').first();
-    }
-
-    function generateValue (sourceFields) {
-      var vals = '';
-      sourceFields.each(function(){
-        vals = vals + $(this).val();
-      });
-      return vals;
-    }
-
-    function fieldAdvance(currentField) {
-      if(!currentField.is(':last-child')) {
-        currentField.next().focus();
-      }
-    }
-
-
-    function copyToDestination (value, destinationField) {
-       destinationField.val(value)
-    }
-
-    function init () {
-      $('[data-replicate-destination]').each(function() {
-        var replicateGroup = $(this).attr('data-replicate-destination'); // Unique name for group
-        // Cache sourceFields
-        var sourceFields = getSourceFields(replicateGroup);
-
-        // Wire up keypress and keyup events
-        sourceFields.keypress(function(e) {
-          var that = $(this);
-          if (that.val().length+1 == that.attr('maxlength') && e.which !== 0) {
-            fieldAdvance(that); // Advance to the next field
-          }
-        }).keyup(function() {
-          copyToDestination(
-            generateValue(sourceFields),
-            getDestinationField(replicateGroup)
-          ); // Copy text to the destination field
-        });
-      })
-    }
-
-  // If we are have replicate-destination present
-  if($('[data-replicate-destination]').length) {
-    init();
+  function ConcatFields() {
+    this.sourceFields = false;
   }
 
-}());
+  ConcatFields.prototype = {
+
+    getSourceFields: function(replicateGroup) {
+      return document.querySelectorAll('[data-replicate-source='+replicateGroup+']');
+    },
+
+    getDestinationField: function(replicateGroup) {
+      return document.querySelector('[data-replicate-destination='+replicateGroup+']');
+    },
+
+    generateValue: function(sourceFields) {
+      var vals = '';
+      for (var i = 0; i < sourceFields.length; i++) {
+        vals = vals + sourceFields[i].value;
+      };
+      return vals;
+    },
+
+    fieldAdvance: function(element) {
+      // maximum number of characters reached?
+      var value = element.value.trim();
+
+      // If there is no more space in that field and another field is a sibling
+      if((value.length == element.maxLength) && element.nextElementSibling) {
+        element.nextElementSibling.focus();
+      }
+    },
+
+    copyToDestination: function(value, destinationField) {
+      destinationField.value = value;
+    },
+
+    init: function(replicateGroup) {
+      var that = this,
+          sourceFields = this.getSourceFields(replicateGroup); // Cache sourceFields
+
+      // Add an event handler to each <input>
+      [].forEach.call(sourceFields, function(element) { // Borrow the forEach method from the 'Array' built in object
+        element.addEventListener("keyup", function(ev) {
+          // Jump to next field
+          that.fieldAdvance(element);
+
+          // Update hidden field
+          that.copyToDestination(
+            that.generateValue(sourceFields),
+            that.getDestinationField(replicateGroup)
+          );
+        });
+      });
+    }
+  }
+
+  // If we have replicate-destination present
+  var elements = document.querySelectorAll('[data-replicate-destination]');
+  if(elements.length) {
+    for (var i = 0; i < elements.length; i++) {
+      var replicateGroup = elements[i].getAttribute('data-replicate-destination'); // Unique name for group
+      var field = new ConcatFields;
+      field.init(replicateGroup);
+    };
+  }
+
+})();
