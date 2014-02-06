d90a08e39385275e671151a34677f20f04750ec6
Ryan Margheriti <ryanmargheriti@gmail.com>
2014-02-06 18:55:45 +1100

    jquery-free refactor

--- scripts/plugins/concat-input-fields2.js
+++ scripts/plugins/concat-input-fields2.js
@@ -8,9 +8,7 @@
   ConcatFields.prototype = {
 
     getSourceFields: function(replicateGroup) {
-      var one = document.querySelectorAll('[data-replicate-source='+replicateGroup+']');
-      var two = $('[data-replicate-source='+replicateGroup+']');
-      return two;
+      return document.querySelectorAll('[data-replicate-source='+replicateGroup+']');
     },
 
     getDestinationField: function(replicateGroup) {
@@ -19,29 +17,20 @@
 
     generateValue: function(sourceFields) {
       var vals = '';
-      for (var i = 0; i < vals.length; i++) {
-        vals = vals + vals[i].value;
+      for (var i = 0; i < sourceFields.length; i++) {
+        vals = vals + sourceFields[i].value;
       };
-      /*sourceFields.each(function(){
-          vals = vals + $(this).val();
-      });*/
       return vals;
     },
 
     fieldAdvance: function(element) {
-      // if(!currentField.is(':last-child')) {
-      //     sourceFields[i+1].focus();
-      // }
-
-      // // do { el = el.nextSibling } while ( el && el.nodeType !== 1 )
-      // return el
       // maximum number of characters reached?
       var value = element.value.trim();
 
-      if(element.nextElementSibling && (value.length == element.maxLength)) {
+      // If there is no more space in that field and another field is a sibling
+      if((value.length == element.maxLength) && element.nextElementSibling) {
         element.nextElementSibling.focus();
       }
-
     },
 
     copyToDestination: function(value, destinationField) {
@@ -49,35 +38,33 @@
     },
 
     init: function(replicateGroup) {
-      // Cache sourceFields
       var that = this,
-          sourceFields = this.getSourceFields(replicateGroup);
-
+          sourceFields = this.getSourceFields(replicateGroup); // Cache sourceFields
 
       // Add an event handler to each <input>
       [].forEach.call(sourceFields, function(element) { // Borrow the forEach method from the 'Array' built in object
         element.addEventListener("keyup", function(ev) {
+          // Jump to next field
           that.fieldAdvance(element);
+
+          // Update hidden field
+          that.copyToDestination(
+            that.generateValue(sourceFields),
+            that.getDestinationField(replicateGroup)
+          );
         });
       });
-
     }
-
   }
 
   // If we have replicate-destination present
-
   var elements = document.querySelectorAll('[data-replicate-destination]');
-
   if(elements.length) {
-
     for (var i = 0; i < elements.length; i++) {
       var replicateGroup = elements[i].getAttribute('data-replicate-destination'); // Unique name for group
       var field = new ConcatFields;
       field.init(replicateGroup);
     };
-
   }
-
 
 })();
