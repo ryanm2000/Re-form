3b0e915a19de45805a2596e4b5ad73a7b854009b
Ryan Margheriti <ryanmargheriti@gmail.com>
2014-02-24 18:52:56 +1100

    IE Compatibility

--- scripts/concat-input-fields.js
+++ scripts/concat-input-fields.js
@@ -15,6 +15,14 @@
       return document.querySelector('[data-replicate-destination='+replicateGroup+']');
     },
 
+    addEvent: function(element, event, func) {
+      if(element.addEventListener) {
+        element.addEventListener(event, func);
+      } else { // IE8
+        element.attachEvent('on'+event, func);
+      }
+    },
+
     generateValue: function(sourceFields) {
       var vals = '';
       for (var i = 0; i < sourceFields.length; i++) {
@@ -25,10 +33,12 @@
 
     fieldAdvance: function(element) {
       // maximum number of characters reached?
-      var value = element.value.trim();
-
+      var value = element.value;
       // If there is no more space in that field and another field is a sibling
-      if((value.length == element.maxLength) && element.nextElementSibling) {
+      if( (value.length == element.maxLength) &&
+          (element.nextElementSibling || nextElementSibling(element))
+      ) {
+        var nextSibling = element.nextElementSibling || nextElementSibling(element);
         element.nextElementSibling.focus();
       }
     },
@@ -42,19 +52,21 @@
           sourceFields = this.getSourceFields(replicateGroup); // Cache sourceFields
 
       // Add an event handler to each <input>
-      // TODO: Add IE8 Compatibility
-      [].forEach.call(sourceFields, function(element) { // Borrow the forEach method from the 'Array' built in object
-        element.addEventListener("keyup", function(ev) {
-          // Jump to next field
-          that.fieldAdvance(element);
+      for (var i = 0, j = sourceFields.length; i < j; i++) {
+        (function(i) {
+          that.addEvent(sourceFields[i], 'keyup', function() {
 
-          // Update hidden field
-          that.copyToDestination(
-            that.generateValue(sourceFields),
-            that.getDestinationField(replicateGroup)
-          );
-        });
-      });
+            // Jump to next field
+            that.fieldAdvance(sourceFields[i]);
+
+            // Update hidden field
+            that.copyToDestination(
+              that.generateValue(sourceFields),
+              that.getDestinationField(replicateGroup)
+            );
+          });
+        })(i);
+      }
     }
   }
 
